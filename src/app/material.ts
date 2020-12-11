9a7d812efde9588dca67e99c4ba2949fc5892524
mikael <michaelajnew@gmail.com>
2020-12-11 16:22:34 +0300

    Added the material design to the app Toolbar component and also added the button with material design

---
+++ src/app/material.ts
@@ -0,0 +1,10 @@
+import { MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+})
+export class MyMaterialModule { }
