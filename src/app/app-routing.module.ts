d37516c73b1c525b825b7025ac0c2c2e76da5e7a
mikael <michaelajnew@gmail.com>
2021-02-13 22:40:07 +0300

    DELETED THE MOH-SOPs COMPONENT FUTURE PLAN IS TO IMPLEMENT THIS ONTO A DIALOG POP UP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UgandaComponent } from './uganda/uganda.component';
-import { MohSopsComponent } from './moh-sops/moh-sops.component';
 
 const routes: Routes = [
   {path: '', component:UgandaComponent},
   {path: 'uganda', component:UgandaComponent},
-  {path: 'home', component:HomeComponent},
-  {path: 'moh_sops', component:MohSopsComponent}
+  {path: 'home', component:HomeComponent}
 ];
 
 @NgModule({
@@ -20,6 +18,5 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [
   UgandaComponent,
-  HomeComponent,
-  MohSopsComponent
+  HomeComponent
 ]
