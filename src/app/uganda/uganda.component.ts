3d3c5faf2aee4272c90e5468643f5e207f17a23b
mikael <michaelajnew@gmail.com>
2021-03-01 22:09:38 +0300

    Added the functions to retrieve individual East African Countries and extracted the API_KEY and API_HOST to  Global variables/properties, Declared and initialised the countries' variables in the constructor

--- src/app/uganda/uganda.component.ts
+++ src/app/uganda/uganda.component.ts
@@ -15,17 +15,28 @@ export class UgandaComponent implements OnInit {
   confirmed_cases : String = "Total confirmed cases";
 
   readonly ROOT_URL = 'https://covid-19-data.p.rapidapi.com/country';
+	readonly API_KEY = 'cc23854be3msh80f7d131de623c6p14400djsnff7e1752faa6';
+	readonly API_HOST = 'covid-19-data.p.rapidapi.com';
 
 	posts: any;
 	
   ngOnInit(): void {
     this.loadFriends();
-		this.loadData();
+		this.loadKeData();
+		this.loadTzData();
+		this.loadRwData();
+		this.loadBuData();
+		this.loadSSData();
+		this.loadSoData();
   }
 
   public friends: Post[];
-
-	public kenyaCases: Post[];
+	public keCases: Post[];
+	public tzCases: Post[];
+	public rwCases: Post[];
+	public buCases: Post[];
+	public ssCases: Post[];
+	public soCases: Post[];
  
 	private apiClient: ApiClient;
  
@@ -33,21 +44,28 @@ export class UgandaComponent implements OnInit {
 	constructor( apiClient: ApiClient ) {
  
 		this.apiClient = apiClient;
+
 		this.friends = [];
-		this.kenyaCases = [];
+		this.keCases = [];
+		this.tzCases = [];
+		this.rwCases = [];
+		this.buCases = [];
+		this.ssCases = [];
+		this.soCases = [];
+	
 	}
 
 	public async loadFriends() : Promise<void> {
  
 		try {
 			this.friends = await this.apiClient.get<Post[]>({
-				url: 'https://covid-19-data.p.rapidapi.com/country',
+				url: this.ROOT_URL,
 				params: {
           name: 'uganda' 
         },
         headers: {
-          'x-rapidapi-key': 'cc23854be3msh80f7d131de623c6p14400djsnff7e1752faa6',
-          'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
+          'x-rapidapi-key': this.API_KEY,
+          'x-rapidapi-host': this.API_HOST
         }
 			});
 		} catch ( error ) {
@@ -55,17 +73,108 @@ export class UgandaComponent implements OnInit {
 		}
 	}
 
-	public async loadData() : Promise<void> {
+	// FUNCTION TO RETRIEVE DATA FOR KENYA
+	public async loadKeData() : Promise<void> {
 		try {
-			this.kenyaCases = await this.apiClient.get<Post[]>({
-				url: 'https://covid-19-data.p.rapidapi.com/country',
+			this.keCases = await this.apiClient.get<Post[]>({
+				url: this.ROOT_URL,
 				params: {
           name: 'kenya' 
         },
         headers: {
-          'x-rapidapi-key': 'cc23854be3msh80f7d131de623c6p14400djsnff7e1752faa6',
-          'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
+          'x-rapidapi-key': this.API_KEY,
+          'x-rapidapi-host': this.API_HOST
         }
+			});
+		} catch ( error ) {
+			console.error( error ); 
+		}
+	}
+
+	// FUNCTION TO RETRIEVE DATA FOR TANZANIA
+	public async loadTzData() : Promise<void> {
+		try {
+			this.keCases = await this.apiClient.get<Post[]>({
+				url: this.ROOT_URL,
+				params: {
+          name: 'tanzania' 
+        },
+        headers: {
+          'x-rapidapi-key': this.API_KEY,
+          'x-rapidapi-host': this.API_HOST
+        }
+			});
+		} catch ( error ) {
+			console.error( error ); 
+		}
+	}
+
+	// FUNCTION TO RETRIEVE RWANDAN DATA/STATS
+	public async loadRwData() : Promise<void> {
+		try {
+			this.keCases = await this.apiClient.get<Post[]>({
+				url: this.ROOT_URL,
+				params: {
+					name: 'rwanda' 
+				},
+				headers: {
+					'x-rapidapi-key': this.API_KEY,
+					'x-rapidapi-host': this.API_HOST
+				}
+			});
+		} catch ( error ) {
+			console.error( error ); 
+		}
+	}
+
+	// FUNCTION TO RETRIEVE BURUNDI DATA/STATS
+	public async loadBuData() : Promise<void> {
+		try {
+			this.keCases = await this.apiClient.get<Post[]>({
+				url: this.ROOT_URL,
+				params: {
+					name: 'burundi' 
+				},
+				headers: {
+					'x-rapidapi-key': this.API_KEY,
+					'x-rapidapi-host': this.API_HOST
+				}
+			});
+		} catch ( error ) {
+			console.error( error ); 
+		}
+	}
+
+	// FUNCTION TO RETRIEVE SOUTH SUDANESE DATA/STATS
+	public async loadSSData() : Promise<void> {
+		try {
+			this.keCases = await this.apiClient.get<Post[]>({
+				url: this.ROOT_URL,
+				params: {
+					name: 'southsudan' 
+				},
+				headers: {
+					'x-rapidapi-key': this.API_KEY,
+					'x-rapidapi-host': this.API_HOST
+				}
+			});
+		} catch ( error ) {
+			console.error( error ); 
+		}
+	}
+
+	// FUNCTION TO RETRIEVE SOMALIA DATA/STATS
+	public async loadSoData() : Promise<void> {
+		try {
+			this.keCases = await this.apiClient.get<Post[]>({
+				url: this.ROOT_URL,
+				params: {
+					name: 'somalia' 
+				},
+				headers: {
+					'x-rapidapi-key': this.API_KEY,
+					'x-rapidapi-host': this.API_HOST
+				}
 			});
 		} catch ( error ) {
 			console.error( error ); 
