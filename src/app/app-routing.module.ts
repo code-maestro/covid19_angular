9b940d771b13bf0a5cb0c7741da8cc7e6cfa1357
mikael <michaelajnew@gmail.com>
2021-03-03 22:33:53 +0300

    Optmised the imports in app-routing.module, Added and Styled the shop UI ppe/products  cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UgandaComponent } from './uganda/uganda.component';
