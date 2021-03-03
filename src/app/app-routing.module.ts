6b9946add4140fa32c3ee78902ddd4e6e19c04a4
mikael <michaelajnew@gmail.com>
2021-03-03 21:43:26 +0300

    Added the shop component into the app-routing.module.ts and add the world data retrieval logoc code into the loader.service.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UgandaComponent } from './uganda/uganda.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component:UgandaComponent},
   {path: 'uganda', component:UgandaComponent},
-  {path: 'home', component:HomeComponent}
+  {path: 'home', component:HomeComponent},
+  {path: 'shop', component:ShopComponent}
 ];
 
 @NgModule({
@@ -16,7 +18,9 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule { }
+
 export const routingComponents = [
   UgandaComponent,
-  HomeComponent
+  HomeComponent,
+  ShopComponent
 ]
--- src/app/loader/loader.service.ts
+++ src/app/loader/loader.service.ts
@@ -1,12 +1,55 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Post } from '../posts';
+import { ApiClient } from '../ApiClient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
 
+  	
+  confirmed_cases : String = "Total confirmed cases";
+
+  readonly WORLD_ROOT_URL = '';
+  readonly API_KEY = 'cc23854be3msh80f7d131de623c6p14400djsnff7e1752faa6';
+  readonly API_HOST = 'covid-19-data.p.rapidapi.com';
+
+  posts: any;
+
+  public worldCases: Post[];
+
+  private apiClient: ApiClient;
+
   public isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  // I initialize the app-component.
+  constructor( apiClient: ApiClient ) {
+  
+    this.apiClient = apiClient;
+  
+    this.worldCases = [];
+
+    this.loadWorldCases();
+
+  }
+  
+  // FUNCTION TO RETRIEVE UGANDAN DATA/STATS
+  public async loadWorldCases() : Promise<void> {
+
+    try {
+      this.worldCases = await this.apiClient.get<Post[]>({
+        url: this.WORLD_ROOT_URL,
+        params: {
+          name: 'uganda' 
+        },
+        headers: {
+          'x-rapidapi-key': this.API_KEY,
+          'x-rapidapi-host': this.API_HOST
+        }
+      });
+    } catch ( error ) {
+      console.error( error ); 
+    }
+  }
 }
